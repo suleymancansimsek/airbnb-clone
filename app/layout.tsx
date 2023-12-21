41f492581e1d26d35ee1e25826cd63a3e8e9bdc6
suleymancansimsek <suleymancansimsek@gmail.com>
2023-12-21 19:05:22 +0300

    layout improvement and modals

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
 import Modal from "./components/modals/Modal";
+import ToasterProvider from "./providers/ToasterProvider";
+import RegisterModal from "./components/modals/RegisterModal";
 
 const font = Nunito({
   subsets: ["latin"],
@@ -23,7 +25,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal/>
+          <ToasterProvider />
+          <RegisterModal/>
         <Navbar/>
         </ClientOnly>
         {children}
