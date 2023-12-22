f2170e2a8c4665e0e320b446d4f2ec7398631d9e
suleymancansimsek <suleymancansimsek@gmail.com>
2023-12-22 23:59:26 +0300

    images - loginmodal

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import ClientOnly from "./components/ClientOnly";
 import Modal from "./components/modals/Modal";
 import ToasterProvider from "./providers/ToasterProvider";
 import RegisterModal from "./components/modals/RegisterModal";
+import LoginModal from "./components/modals/LoginModal";
 
 const font = Nunito({
   subsets: ["latin"],
@@ -26,6 +27,7 @@ export default function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <LoginModal/>
           <RegisterModal/>
         <Navbar/>
         </ClientOnly>
